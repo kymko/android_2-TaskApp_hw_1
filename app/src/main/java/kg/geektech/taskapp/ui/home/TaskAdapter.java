336ebf567d5c59f66ae40812a26cec115d83a34e
kumush <kymkobek@gmail.com>
2021-05-18 12:10:10 +0600

    Home work done

--- app/src/main/java/kg/geektech/taskapp/ui/home/TaskAdapter.java
+++ app/src/main/java/kg/geektech/taskapp/ui/home/TaskAdapter.java
@@ -1,16 +1,23 @@
 package kg.geektech.taskapp.ui.home;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+
 import kg.geektech.taskapp.R;
 import kg.geektech.taskapp.models.Task;
-
 
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
@@ -19,12 +26,11 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
     View view;
 
 
-
     @NonNull
     @NotNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_task,parent,false);
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_task, parent, false);
 
         return new ViewHolder(view);
     }
@@ -32,12 +38,37 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
         holder.bind(list.get(position));
-        
+
         if (position % 2 == 1) {
             view.setBackgroundColor(Color.BLUE);
         } else {
             view.setBackgroundColor(Color.CYAN);
         }
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(v.getContext());
+                alertDialog.setMessage("Вы действительно хотите удалить?");
+                alertDialog.setTitle("Внимание!");
+                alertDialog.setPositiveButton("Да!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        list.remove(position);
+                        notifyDataSetChanged();
+                    }
+                });
+
+                alertDialog.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertDialog.show();
+                return false;
+            }
+        });
+
     }
 
     @Override
@@ -50,7 +81,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
         notifyItemInserted(list.indexOf(task));
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView textTitle;
 
         public ViewHolder(@NonNull @NotNull View itemView) {
@@ -60,6 +91,13 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
 
         public void bind(Task task) {
             textTitle.setText(task.getTitle());
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(view.getContext(), "Position: " + String.valueOf(getAdapterPosition()) + " text: " + task.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
+
 }
